6cf4c58328c690604b6757846f4b0e2d0bbf1c47
mromeroacamica <mgr_89@hotmail.com>
2020-02-19 15:40:20 -0300

    orderLimit

--- servidor/controladores/controlador.js
+++ servidor/controladores/controlador.js
@@ -16,53 +16,102 @@ function buscarTodasPeliculas(req, res) {
     console.log('aaaaaaaaaaaaaaaaaaaaaaaa', req.query)
     let query = req.query;
     let sql;
-    
+    let select = 'select * ';
+    let busqueda;
+
     // console.log(query.genero)
     // console.log(query.anio)
     // console.log(query.titulo)
     if (query.genero && query.anio && query.titulo) {
-
-        sql = 'select * from pelicula where genero_id = "' + query.genero + '" and anio ="' + query.anio + '" and titulo like "%' + query.titulo + '%"';
+        busqueda = 'from pelicula where genero_id = "' + query.genero + '" and anio ="' + query.anio + '" and titulo like "%' + query.titulo + '%"';
+        sql = select + busqueda;
 
     } else if (query.genero && query.anio) {
-        sql = 'select * from pelicula where genero_id = "' + query.genero + '" and anio ="' + query.anio + '"';
+        busqueda = 'from pelicula where genero_id = "' + query.genero + '" and anio ="' + query.anio + '"';
+        sql = select + busqueda;
 
     } else if (query.genero && query.titulo) {
-        sql = 'select * from pelicula where genero_id = "' + query.genero + '" and titulo like "%' + query.titulo + '%"';
+        busqueda = 'from pelicula where genero_id = "' + query.genero + '" and titulo like "%' + query.titulo + '%"';
+        sql = select + busqueda;
 
     } else if (query.genero) {
-        sql = 'select * from pelicula where genero_id = "' + query.genero + '"';
+        busqueda = 'from pelicula where genero_id = "' + query.genero + '"';
+        sql = select + busqueda;
 
     } else if (query.anio && query.titulo) {
-        sql = 'select * from pelicula where anio ="' + query.anio + '" and titulo like "%' + query.titulo + '%"';
+        busqueda = 'from pelicula where anio ="' + query.anio + '" and titulo like "%' + query.titulo + '%"';
+        sql = select + busqueda;
 
     } else if (query.anio) {
+        busqueda = 'from pelicula where anio ="' + query.anio + '"';
+        sql = select + busqueda;
 
-        sql = 'select * from pelicula where anio ="' + query.anio + '"';
-    }else if(query.titulo){
-        sql = 'select * from pelicula where titulo like "%' + query.titulo + '%"';
+    } else if (query.titulo) {
+        busqueda = 'from pelicula where titulo like "%' + query.titulo + '%"';
+        sql = select + busqueda;
 
     } else {
-        sql = 'select * from pelicula';
+        busqueda = 'from pelicula';
+        sql = select + 'from pelicula';
 
     }
 
+    let sql2;
 
-    con.query(sql, function (err, results, fields) {
-        console.log('bbbbbbbbbbbbbbbbbbbb', sql)
-        if (err) {
-            console.log('hubo un error');
-            return res.status(404).send('hubo error');
-        }
-        let response = {
-            peliculas: results
-        }
+    switch (query.columna_orden) {
+        case 'titulo':
+            sql2 = ' ORDER by titulo';
+            break;
+        case 'anio':
+            sql2 = ' ORDER by anio desc';
+            break;
+        case 'puntuacion':
+            sql2 = ' ORDER by puntuacion desc';
+            break;
+        default:
+            sql2 = ' ORDER by titulo';
 
-        // console.log(response)
-        res.send(response)
-        // res.send(JSON.stringify(response));
 
-    })
+    }
+    let cantidad = query.cantidad;
+    let limiteInferior = (query.pagina - 1) * 52;
+    let limite = ' limit ' + limiteInferior + ',' + 52;
+// console.log(limite)
+
+    let sql3 = sql + sql2 + limite;
+    let sql4 = 'select count(id) AS count ' + busqueda
+    let cantidadPeliculasTotal;
+    // console.log('cccccccccccccc',busqueda)
+
+    new Promise((resolve, reject) => {
+
+        con.query(sql4, function (err, results, fields) {
+            // console.log('gggggggggg', sql4)
+            if (err) reject(err);
+            resolve(results)
+        })
+    }).then(response => {
+        cantidadPeliculasTotal = response[0];
+        new Promise((resolve, reject) => {
+            con.query(sql3, function (err, results, fields) {
+                console.log('bbbbbbbbbbbbbbbbbbbb', sql3)
+                if (err) reject(err);
+                resolve(results)
+            })
+        }).then(response => {
+            let respuestaEnviar = {
+                peliculas: response,
+                total: cantidadPeliculasTotal.count
+            }
+            // console.log('llllllllllllll',respuestaEnviar.total);
+            res.send(respuestaEnviar)
+        })
+
+
+
+    });
+
+
 };
 
 function buscarTodosGeneros(req, res) {
